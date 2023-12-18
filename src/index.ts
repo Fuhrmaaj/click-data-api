f03e670e3aea67d9531d410b247a80947c6db6d6
AJ Fuhrmann <Fuhrmaaj@users.noreply.github.com>
2023-12-18 14:41:51 -0500

    Split code into modules (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,11 @@
 import * as fs from 'fs';
+import { reformatter } from './reformatter.js';
 
 // Read the input .cdv provided by the user
-let data = fs.readFileSync('./test_data/test.cdv', 'utf8');
+export let data: string = fs.readFileSync('./test_data/test.cdv', 'utf8');
 
-/* Remove the garbage which prevents the data from being read as a .csv */
-// Remove the header line
-const regexHeader = /^(.*)\n/;
-
-// Remove all the null characters
-const regexNullChars = /\0/g;
-
-// Remove repeat lines
-const regexLines = /(\n|\r)(\n|\r)/g;
-
-data = data.replaceAll(regexNullChars, '')
-    .replaceAll(regexLines, '\n')
-    .replace(regexHeader, '');
+// Convert the data into a .csv readable format
+data = reformatter({ data });
 
 // Write the modified data to a .csv
 fs.writeFileSync('./test_data/test.csv', data);
