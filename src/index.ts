bd1232e2df88f207813117a0461a52fca7db60ff
AJ Fuhrmann <fuhrmaaj@gmail.com>
2023-12-29 11:32:26 -0500

    Allow the user to name the output file.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import * as fs from 'fs';
 import { dataCDV } from './read-file.js';
 import { reformatter } from './reformatter.js';
+import { writeCSV } from './write-file.js';
 
 // Read the data file provided by the user and convert it into a .csv readable format
 const data: string = reformatter(dataCDV);
 
 // Write the modified data to a .csv
-fs.writeFileSync('./log_data/test.csv', data);
+writeCSV(data);
---
+++ src/write-file.ts
@@ -0,0 +1,54 @@
+import * as fs from 'fs';
+import * as rl from 'readline-sync';
+
+const userQuery = (): string => {
+  return rl.question('Please choose a name for the output file.\n');
+};
+
+// Make sure the user has chosen a valid file output name
+function filenameCheck(name: string): boolean {
+  // Forbidden characters \ / : * ? " < > | and also don't allow .
+  const forbiddenChars = /^[^\\/:*?"<>|.]+$/;
+
+  if (!forbiddenChars.test(name)) {
+    console.log('File names may not include \\ / : * ? " < > | or .');
+    return false;
+  }
+
+  // Forbidden filenames
+  const forbiddenNames = /^(CON|PRN|AUX|NUL|COM[0-9]|LPT[0-9])$/;
+
+  if (forbiddenNames.test(name)) {
+    console.log(`${fileDestination} is a forbidden filename in Windows.`);
+    return false;
+  }
+
+  // Disallow whitespace
+  const whitespace = /\s/;
+
+  if (whitespace.test(name)) {
+    console.log('File name may not have spaces. Use - or _ instead.');
+    return false;
+  }
+  return true;
+}
+
+// Ask the user to name the output file
+let fileDestination: string = userQuery();
+
+// Test that the output name is valid
+let nameTest: boolean = false;
+
+while (nameTest == false) {
+  fileDestination = userQuery();
+  nameTest = filenameCheck(fileDestination);
+}
+
+// Append ".csv" to the end of the file name
+fileDestination += '.csv';
+
+// Write the modified data to to disk
+export const writeCSV = (data: string): void => {
+  fs.writeFileSync(`./log_data/${fileDestination}`, data);
+  return console.log('File successfully converted!');
+};
